fa0fa5e0be1b2ef8d68f9c5b8f9dc82401b7545d
jules1952 <jrojekr@gmail.com>
2025-05-20 14:27:31 +0200

    Dodano docstringi do utils.py (poprawa Pylint)

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
+"""Moduł zawierający podstawowe operacje matematyczne."""
+
 def add(a, b):
+    """Zwraca sumę dwóch liczb."""
     return a + b
 
 def subtract(a, b):
+    """Zwraca różnicę dwóch liczb."""
     return a - b
 
 def multiply(a, b):
+    """Zwraca iloczyn dwóch liczb."""
     return a * b
 
 def divide(a, b):
+    """Zwraca iloraz dwóch liczb."""
     return a / b
